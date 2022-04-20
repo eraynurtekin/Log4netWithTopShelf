da114d0a3f771ce263e42d739cf7b1746795fc92
Eray Nurtekin <79145084+eraynurtekin@users.noreply.github.com>
2022-04-20 12:48:13 +0300

    Add project files.

---
+++ Try3/Program.cs
@@ -0,0 +1,39 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Try3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            
+            XmlConfigurator.Configure();
+
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<TimerService>(s =>
+                {
+                    s.ConstructUsing(name => new TimerService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.UseLog4Net();
+
+                x.SetDescription("Sample Topshelf Host");
+                x.SetDisplayName("Timer Service");
+                x.SetServiceName("TimerService");
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ Try3/TimerService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Try3
+{
+    public class TimerService
+    {    
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+        readonly Timer _timer;
+        public TimerService()
+            {
+            //WriteMessage();
+            _timer = new Timer(1000) { AutoReset = true };
+
+            // _timer.Elapsed += (sender, eventArgs) => File.AppendAllLines(@"C:\temp\tryLog\timer.log", new string[] { string.Format("It is {0} and all is well", DateTime.Now) });
+            _timer.Elapsed += TimerElapsed;
+
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+
+            //File.AppendAllLines(@"C:\temp\tryLog\timer.log", lines);
+            //message = lines[0];
+            log.Warn(lines);
+        }
+
+
+        //private void WriteMessage()
+        //{
+        //    log.Info("Merhaba Alim");
+        //    log.Error("Hata yapıyoruz");
+        //}
+
+
+
+        public void Start() { _timer.Start(); }
+            public void Stop() { _timer.Stop(); }
+        }
+    
+}
